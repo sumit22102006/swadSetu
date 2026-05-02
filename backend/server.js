83e2c2851818246526dbb86cf5ce4cf340635cc9
sumit22102006 <sumitkryaduvanshi@gmail.com>
2026-05-02 17:13:38 +0530

    fix the server issue

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,12 @@ connectDB().catch(err => {
 
 const app = express();
 
+// Request Logger for debugging
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
